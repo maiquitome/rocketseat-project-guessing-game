a7e45cca144740b3d2b427ba30f865de6f2ac3d7
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-06 09:42:37 -0300

    feat: :sparkles: add `Letter` and `.word`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Tip } from "./components/Tip";
 import styles from "./app.module.css";
 
+import { Letter } from "./components/Letter";
 import { Header } from "./components/Header";
 
 function App() {
@@ -13,6 +14,13 @@ function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Dica de teste" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   );
