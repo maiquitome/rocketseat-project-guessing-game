3b3560869b5888e782de6b89f023dc0c5361caab
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-07 15:33:28 -0300

    feat: :sparkles: implement `handleRestartGame()`

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,13 @@ function App() {
   const [challenge, setChallenge] = useState<Challenge | null>(null);
 
   function handleRestartGame() {
-    alert("Reiniciar o jogo!");
+    const isConfirmed = window.confirm(
+      "Você tem certeza que deseja reiniciar?"
+    );
+
+    if (isConfirmed) {
+      startGame();
+    }
   }
 
   function startGame() {
